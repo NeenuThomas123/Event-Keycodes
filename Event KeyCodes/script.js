abf4dc7318ccb10a89154c88c5b8b677aba2ae1b
NeenuThomas123 <neenukaduvanal123@gmail.com>
2025-06-28 08:34:28 +0530

    Add files via upload

---
+++ Event KeyCodes/script.js
@@ -0,0 +1,24 @@
+//get html insert
+const insert = document.getElementById('insert')
+
+//listen for any key press anywhere on the page
+window.addEventListener('keydown', (event) => {
+
+  //inside that function build three little boxes
+  insert.innerHTML = `
+  <div class="key">
+  ${event.key === ' ' ? 'Space' : event.key} 
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>
+  `
+})
